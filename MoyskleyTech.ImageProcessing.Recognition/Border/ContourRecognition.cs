cc66a951851937b22231ba9346469d65830f1c31
Jonathan Boisclair <boiscljo@uqtr.ca>
2018-06-08 09:58:34 -0400

    Hu Moments

--- MoyskleyTech.ImageProcessing.Recognition/Border/ContourRecognition.cs
+++ MoyskleyTech.ImageProcessing.Recognition/Border/ContourRecognition.cs
@@ -89,7 +89,7 @@ namespace MoyskleyTech.ImageProcessing.Recognition.Border
             return contours;
         }
         public static List<Contour<Representation>> Analyse<Representation>(ImageProxy<Representation> bmp , Func<Representation , bool> condition , bool keepAllPoints = false , ContourRecognitionMode mode = ContourRecognitionMode.EightConnex)
-            where Representation:struct
+            where Representation : struct
         {
             HashSet<Point> visited=new HashSet<Point>();
             List<Contour< Representation >> contours = new List<Contour<Representation>>();
@@ -167,6 +167,135 @@ namespace MoyskleyTech.ImageProcessing.Recognition.Border
             }
             return contours;
         }
+
+        public static double[ ] GetHuMoments<Representation>(ImageProxy<Representation> proxy)
+            where Representation : struct
+        {
+            var converter = ColorConvert.GetConversionFrom<Representation,Pixel>();
+            var grayImage = proxy.ToImage((r)=>converter(r).GetGrayTone());
+            //Called 3 times
+            double getRawMoment(int p , int q)
+            {
+                int width = grayImage.Width;
+                int height = grayImage.Height;
+
+                double m = 0;
+                for ( int i = 0, k = height; i < k; i++ )
+                {
+                    for ( int j = 0, l = width; j < l; j++ )
+                    {
+                        m += Math.Pow(i , p) * Math.Pow(j , q) * grayImage.Get(i , j);
+                    }
+                }
+                return m;
+            }
+            //Called 1 time
+            PointF getCentroid()
+            {
+                double m00 = getRawMoment(0, 0);
+                double m10 = getRawMoment(1, 0);
+                double m01 = getRawMoment(0, 1);
+                double x0 = m10 / m00;
+                double y0 = m01 / m00;
+                return new PointF(x0 , y0);
+            }
+            PointF centroid = getCentroid();
+            //Called 8 times
+            double getCentralMoment(int p , int q)
+            {
+                int width = grayImage.Width;
+                int height = grayImage.Height;
+
+                double mc = 0;
+                for ( int i = 0, k = height; i < k; i++ )
+                {
+                    for ( int j = 0, l = width; j < l; j++ )
+                    {
+                        mc += Math.Pow(( i - centroid.X ) , p) * Math.Pow(( j - centroid.Y ) , q) * grayImage.Get(i , j);
+                    }
+                }
+                return mc;
+            }
+            //Called 8 times
+            double getNormalizedCentralMoment(int p , int q)
+            {
+                double gama = ((p + q) / 2) + 1;
+                double mpq = getCentralMoment(p, q);
+                double m00gama = Math.Pow(mpq, gama);
+                return mpq / m00gama;
+            }
+
+            double[] moments = new double[8];
+
+            double
+            n20 = getNormalizedCentralMoment( 2, 0),
+            n02 = getNormalizedCentralMoment( 0, 2),
+            n30 = getNormalizedCentralMoment(3, 0),
+            n12 = getNormalizedCentralMoment(1, 2),
+            n21 = getNormalizedCentralMoment( 2, 1),
+            n03 = getNormalizedCentralMoment( 0, 3),
+            n11 = getNormalizedCentralMoment( 1, 1);
+
+            //First moment
+            moments[0] = n20 + n02;
+
+            //Second moment
+            moments[1] = Math.Pow(( n20 - 02 ) , 2) + Math.Pow(2 * n11 , 2);
+
+            //Third moment
+            moments[2] = Math.Pow(n30 - ( 3 * ( n12 ) ) , 2)
+                       + Math.Pow(( 3 * n21 - n03 ) , 2);
+
+            //Fourth moment
+            moments[3] = Math.Pow(( n30 + n12 ) , 2) + Math.Pow(( n12 + n03 ) , 2);
+
+            //Fifth moment
+            moments[4] = ( n30 - 3 * n12 ) * ( n30 + n12 )
+                         * ( Math.Pow(( n30 + n12 ) , 2) - 3 * Math.Pow(( n21 + n03 ) , 2) )
+                         + ( 3 * n21 - n03 ) * ( n21 + n03 )
+                         * ( 3 * Math.Pow(( n30 + n12 ) , 2) - Math.Pow(( n21 + n03 ) , 2) );
+
+            //Sixth moment
+            moments[5] = ( n20 - n02 )
+                         * ( Math.Pow(( n30 + n12 ) , 2) - Math.Pow(( n21 + n03 ) , 2) )
+                         + 4 * n11 * ( n30 + n12 ) * ( n21 + n03 );
+
+            //Seventh moment
+            moments[6] = ( 3 * n21 - n03 ) * ( n30 + n12 )
+                         * ( Math.Pow(( n30 + n12 ) , 2) - 3 * Math.Pow(( n21 + n03 ) , 2) )
+                         + ( n30 - 3 * n12 ) * ( n21 + n03 )
+                         * ( 3 * Math.Pow(( n30 + n12 ) , 2) - Math.Pow(( n21 + n03 ) , 2) );
+
+            //Eighth moment
+            moments[7] = n11 * ( Math.Pow(( n30 + n12 ) , 2) - Math.Pow(( n03 + n21 ) , 2) )
+                             - ( n20 - n02 ) * ( n30 + n12 ) * ( n03 + n21 );
+
+            grayImage.Dispose();
+            return moments;
+        }
+        public static double HuDistance<Representation1,Representation2>(ImageProxy<Representation1> A, ImageProxy<Representation2> B,int noDistance)
+             where Representation1 : struct
+             where Representation2 : struct
+        {
+            var hu1 = GetHuMoments(A);
+            var hu2 = GetHuMoments(B);
+
+            var ma1 = (from x in hu1 select Math.Sign(x) * Math.Log10(x)).ToArray();
+            var ma2 = (from x in hu2 select Math.Sign(x) * Math.Log10(x)).ToArray();
+            if ( noDistance == 1 )
+            {
+                return ( from i in Enumerable.Range(0 , hu1.Length) select 1d / ma1[i] - 1d / ma2[i] ).Sum();
+            }
+            else if ( noDistance == 2 )
+            {
+                return ( from i in Enumerable.Range(0 , hu1.Length) select ma1[i] - ma2[i] ).Sum();
+            }
+            else if ( noDistance == 3 )
+            {
+                return ( from i in Enumerable.Range(0 , hu1.Length) select (ma1[i] - ma2[i])/ ma1[i] ).Sum();
+            }
+            return double.NaN;
+        }
     }
     public enum ContourRecognitionMode
     {
@@ -212,7 +341,7 @@ namespace MoyskleyTech.ImageProcessing.Recognition.Border
                     if ( tmp2[tx , ty] )
                     {
                         found = true;
-                        Points.AddRange(tmp2.Match8ConnexList(tx , ty).Select((pt) => new Point(pt.X+rct.Left,pt.Y+rct.Top)));
+                        Points.AddRange(tmp2.Match8ConnexList(tx , ty).Select((pt) => new Point(pt.X + rct.Left , pt.Y + rct.Top)));
                     }
                 }
             }
@@ -232,7 +361,7 @@ namespace MoyskleyTech.ImageProcessing.Recognition.Border
         }
     }
     public class Contour<Representation>
-        where Representation:struct
+        where Representation : struct
     {
         public Contour()
         {
