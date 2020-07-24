59867f1f4c8c52a2437399b8048ef2ede41593d9
boiscljo <boiscljo@uqtr.ca>
2020-07-24 08:51:26 -0400

    Allow the publish to set a new number

--- publish-packages.ps1
+++ publish-packages.ps1
@@ -1,19 +1,32 @@
 #!/usr/bin/pwsh
+param(
+    [Parameter(Mandatory)]
+    [string]$version
+)
+foreach($d in (gci . -Directory))
+{
+  $csproj = gci "$($d.FullName)/*.csproj"
+  Write-Output $csproj.FullName
+  $content = Get-Content $csproj.FullName
+  $v = $content -replace "(<version>)[0-9.]+(</version>)", "<Version>$version</Version>"
+  Set-Content $csproj.FullName $v
+}
 dotnet build --configuration Release
 dotnet pack --configuration Release
 foreach($d in (gci . -Directory))
 {
   Set-Location $d.FullName
-  $nugetPath="bin/MCD/Release/$($d.Name).1.0.0.nupkg"
+  $nugetPath="bin/MCD/Release/$($d.Name).$version.nupkg"
   if(Test-Path $nugetPath)
   {
     Write-Output $nugetPath
     dotnet nuget push "$nugetPath" --source "github"
   }
-  $nugetPath="bin/Release/$($d.Name).1.0.0.nupkg"
+  $nugetPath="bin/Release/$($d.Name).$version.nupkg"
   if(Test-Path $nugetPath)
   {
     Write-Output $nugetPath
     dotnet nuget push "$nugetPath" --source "github"
   }
+  
 }
